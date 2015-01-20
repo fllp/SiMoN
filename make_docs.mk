e734f8d54bd395fbfdd08dd14e6f5302e6e23e24
Flying Lollipop <s.herteis@campus.lmu.de>
2015-01-20 17:33:29 +0100

    Rename Make_Docs.mk. Implement suggestions from comments in 1d27ea30d

--- make_docs.mk
+++ make_docs.mk
@@ -1,6 +1,13 @@
 BIBLIO = doc/bib/references.bib
 REFSTYLE = doc/bib/ACL.csl
 H_INC = doc/pandoc-h-inc.tex
+INKSCAPE_CMD ?= inkscape
+INKSC_OPTS ?= -C -d=300
+INKSCAPE ?= $(INKSCAPE_CMD) $(INKSC_OPTS)
+
+# little hack to have variable SPACE contain a whitespace
+EMPTY :=
+SPACE := $(EMPTY) $(EMPTY)
 
 # setting flags and extensions for pandoc to use on documentation files
 define pandoc_flags
@@ -22,7 +29,6 @@ endef
 # --listings \
 # -V monofont="Dejavu Sans Mono - Book" \
 
-
 define pandoc_extensions
  fenced_code_blocks\
  yaml_metadata_block\
@@ -36,25 +42,24 @@ endef
 #  pandoc_title_block\
 
 # search for .pandoc files
-doc_files = $(notdir $(shell find doc -type f -name '*.pandoc' | sort -n));
+doc_files = $(notdir $(shell find doc -type f -name '*.pandoc' | sort -n))
 
 # search for .svg files
-image_names = $(basename $(shell find doc/img -type f -name '*.svg' -printf '%p ') );
+svg_sources = $(basename $(shell find doc/img -type f -iname '*.svg' -printf '%p ') )
+# create names of target PNGs
+png_exports = $(addsuffix .png, $(svg_sources))
 
 vpath %.pandoc doc/
-docs: SiMoN-Documentation.pdf # graphics
 
-# little hack to have variable SPACE contain a whitespace
-EMPTY :=
-SPACE := $(EMPTY) $(EMPTY)
+docs: SiMoN-Documentation.pdf
+# SiMoN-Documentation.pdf: graphics
+
+graphics: $(png_exports)
 
 %.pdf: $(doc_files)
+	@echo $(doc_files)
 	@pandoc $(pandoc_flags) -o $@ -f markdown$(subst $(SPACE),+,$(pandoc_extensions)) $^
 	@echo 'Documentation written to' $@
 
-graphics: $(addsuffix .png, $(image_names))
-	@echo $^ $(image_names)
-
-%.png:
-	inkscape -C -d=300 -f $(basename $@).svg -e $@
-	
+%.png: %.svg
+	$(INKSCAPE) -f $< -e $@
